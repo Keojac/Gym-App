928f00bbba68f2325b88f549a1be87a51527ecfc
Kevin Jacob <kevinjac99@gmail.com>
2022-07-14 21:44:30 +1000

    seeded app with exercises

--- seed/base-exercises.js
+++ seed/base-exercises.js
@@ -3,14 +3,70 @@ module.exports = [
         "name": "Squat",
         "targets": ["Compound"],
         "form": ["Start with your hips"],
-        "images": "https://img.freepik.com/free-photo/male-athlete-with-face-mask-using-hand-weight-while-doing-squats-health-club_637285-8591.jpg?t=st=1657773953~exp=1657774553~hmac=eb04016cceffa2d85b0e2f2120fae661b12db61ab8da0689156bad7fcad5d3d8&w=996",
+        "images": "https://res.cloudinary.com/dlbeiziwa/image/upload/v1657796975/Gym%20App%20Pictures/male-athlete-with-face-mask-using-hand-weight-while-doing-squats-health-club_637285-8591_ofq39q.jpg",
         "user_id": "62ce4e74858c9d003b81e4d5"
     },
     {
         "name": "Bench Press",
         "targets": ["Chest"],
         "form": ["Bring the bar to your chest"],
-        "images": "https://img.freepik.com/free-photo/focused-man-doing-workout-weight-bench_329181-14155.jpg?t=st=1657774194~exp=1657774794~hmac=4ef446ed5a05f8b9c8e34f7e5fde36804c4f6c0216dd1cfbfa79aa45ae02e5eb&w=996",
+        "images": "https://res.cloudinary.com/dlbeiziwa/image/upload/v1657797040/Gym%20App%20Pictures/focused-man-doing-workout-weight-bench_329181-14155_f8w4ay.jpg",
+        "user_id": "62ce4e74858c9d003b81e4d5"
+    },
+    {
+        "name": "Dead Lift",
+        "targets": ["Compound"],
+        "form": ["Keep a flat back"],
+        "images": "https://res.cloudinary.com/dlbeiziwa/image/upload/v1657797237/Gym%20App%20Pictures/muscular-build-man-performing-deadlift-while-having-weight-training-gym_637285-2492_bvltnr.jpg",
+        "user_id": "62ce4e74858c9d003b81e4d5"
+    },
+    {
+        "name": "Chest Fly",
+        "targets": ["Chest"],
+        "form": ["Arms in line with front of chest"],
+        "images": "https://res.cloudinary.com/dlbeiziwa/image/upload/v1657797488/Gym%20App%20Pictures/attractive-muscular-man-is-looking-camera-while-doing-stretchening-with-rubber_613910-20892.jpg_buutyx.jpg",
+        "user_id": "62ce4e74858c9d003b81e4d5"
+    },
+    {
+        "name": "Shoulder Press",
+        "targets": ["Shoulders"],
+        "form": ["Keep arms at a 30 degree angle"],
+        "images": "https://res.cloudinary.com/dlbeiziwa/image/upload/v1657797684/Gym%20App%20Pictures/bodybuilder-black-shorts-holding-dumbell-studio-shoot-grey-background_613910-15388.jpg_yrwuut.jpg",
+        "user_id": "62ce4e74858c9d003b81e4d5"
+    },
+    {
+        "name": "Cable Tricep Extension",
+        "targets": ["Triceps"],
+        "form": ["Keep elbows into your sides"],
+        "images": "https://res.cloudinary.com/dlbeiziwa/image/upload/v1657797959/Gym%20App%20Pictures/athletic-blonde-woman-sportswear-doing-exercise-triceps-crossover-machine-gym_613910-18533_illt1n.jpg",
+        "user_id": "62ce4e74858c9d003b81e4d5"
+    },
+    {
+        "name": "Pull Ups",
+        "targets": ["Back"],
+        "form": ["Squeeze your lats till your chin is over the bar"],
+        "images": "https://res.cloudinary.com/dlbeiziwa/image/upload/v1657798076/Gym%20App%20Pictures/concept-power-strength-healthy-lifestyle-sport-powerful-attractive-muscular-man-gym_155003-4993.jpg_qkjuq5.jpg",
+        "user_id": "62ce4e74858c9d003b81e4d5"
+    },
+    {
+        "name": "Preacher Curls",
+        "targets": ["Biceps"],
+        "form": ["Hold barbell at arms length down in front of you"],
+        "images": "https://res.cloudinary.com/dlbeiziwa/image/upload/v1657798153/Gym%20App%20Pictures/pretty-young-woman-lifting-barbell-front-mirror-gym_662251-1118_tiadlm.jpg",
+        "user_id": "62ce4e74858c9d003b81e4d5"
+    },
+    {
+        "name": "Weighted Lunges",
+        "targets": ["Legs"],
+        "form": ["Bend at knee until thigh approaches parallel to the ground"],
+        "images": "https://res.cloudinary.com/dlbeiziwa/image/upload/v1657798404/Gym%20App%20Pictures/woman-does-exercising-outdoors-dressed-sport-costume-wears-sneakers-poses-city-uses-karemat-fitness-training_273609-53924_d6yyf4.jpg",
+        "user_id": "62ce4e74858c9d003b81e4d5"
+    },
+    {
+        "name": "Leg Raise",
+        "targets": ["Core"],
+        "form": ["Raise your straight legs outward in front of you"],
+        "images": "https://res.cloudinary.com/dlbeiziwa/image/upload/v1657798860/Gym%20App%20Pictures/tensed-man-doing-hanging-leg-raises-seaside_1262-6308.jpg_i0tmfg.jpg",
         "user_id": "62ce4e74858c9d003b81e4d5"
     },
 ]
